3a2f47fd0c5078b67cb538f6c032d3a9a072319a
shubhamkr01 <skshubham2014@gmail.com>
2020-08-05 12:20:14 +0530

    Update adjacency_list.cpp
    alignment

--- Graphs/adjacency_list.cpp
+++ Graphs/adjacency_list.cpp
@@ -7,55 +7,56 @@ using namespace std;
 class Graph
 {
   int v; //no of vertices
-	vector<list<int>> adj; // adjacency list for each vertex
+  vector<list<int>> adj; // adjacency list for each vertex
 public:
-	void addEdge(int u, int v);
-	void setV(int n);
-	void printGraph();
+  void addEdge(int u, int v);
+  void setV(int n);
+  void printGraph();
 };
 
 
 void Graph::setV(int n)
 {
-	this->v = n;
-	adj.resize(n);
-	return;
+  this->v = n;
+  adj.resize(n);
+  return;
 }
 
 void Graph::addEdge(int u, int v)
 {
-	auto& li = this->adj;
-	li[u].push_back(v);
-	return;
+  auto& li = this->adj;
+  li[u].push_back(v);
+  return;
 }
 
 void Graph::printGraph()
 {
-	for(int i = 0; i < v; i++)
-	{
-		cout<<" Adjacency list for " << i << " is : head";
-		for(auto j : adj[i])
-		{
-			cout << "-> " << j ;
-		}
-	cout << endl;
-	}
+  for(int i = 0; i < v; i++)
+  {
+    cout<<" Adjacency list for " << i << " is : head";
+    for(auto j : adj[i])
+    {
+      cout << "-> " << j ;
+    }
+  cout << endl;
+  }
+  return;
 }
 
 int main()
 {
-	Graph g;
+  Graph g;
 
-	g.setV(5);
-	g.addEdge(0, 1);
-	g.addEdge(0, 3);
-	g.addEdge(1, 2);
-	g.addEdge(1, 3);
-	g.addEdge(2, 4);
-	g.addEdge(0, 4);
-	g.addEdge(3, 4);
-	g.addEdge(4, 0);
-	g.printGraph();
-	
+  g.setV(5);
+  g.addEdge(0, 1);
+  g.addEdge(0, 3);
+  g.addEdge(1, 2);
+  g.addEdge(1, 3);
+  g.addEdge(2, 4);
+  g.addEdge(0, 4);
+  g.addEdge(3, 4);
+  g.addEdge(4, 0);
+  g.printGraph();
+  
   return 0;
 }
